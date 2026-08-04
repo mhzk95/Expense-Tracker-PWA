aac4e0058881e22cc6dc5c2643855b29e2a7935e
muhammad sakkeer <muhammad.s@evalogical.com>
2026-08-04 17:12:02 +0530

    feat: integrate Microsoft Clarity tracking script into layout and update service worker logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} h-full`}>
       <head>
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xx3d1v7hkw");
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
